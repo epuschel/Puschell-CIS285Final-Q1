1866f002e5c8c203d3bcdabf5af4fcb0d1ddee70
epuschel <73982403+epuschel@users.noreply.github.com>
2021-12-16 16:43:30 -0500

    Fixed SelectionSort.java

--- SelectionSort.java
+++ SelectionSort.java
@@ -3,41 +3,22 @@ public class SelectionSort {
          /* Selection Sort function */
 
     public static void sort( int arr[] ){
-
+    	
         int N = arr.length;
-
-        int i, j, pos, temp;
+        int i, j, temp;
 
         for (i = 0; i < N; i++)
-
         {
-
-            pos = j;
-
-            for (j = i+1; j < N-1; j++)
-
+            for (j = i+1; j < N; j++)
             {
-
-                if (arr[j] < arr[pos])
-
+                if (arr[j] < arr[i])
                 {
-
-                    pos = i;
-
+                	/* Swap arr[i] and arr[pos] */
+                	temp = arr[i];
+                    arr[i] = arr[j];
+                    arr[j]= temp;
                 }
-
-            }
-
-            /* Swap arr[i] and arr[pos] */
-
-            temp = arr[I];
-
-            arr[i] = arr[pos];
-
-            arr[pos]= temp;           
-
+            }          
         }       
-
       }
-
 }
